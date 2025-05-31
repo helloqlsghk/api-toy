839bff096c816eecda692a87fa41866256abfb66
helloqlsghk@naver.com <helloqlsghk@naver.com>
2025-05-31 10:45:52 +0900

    Add .gitkeep to track empty folders

---
+++ src/main/java/com/api/apitoy/ApiToyApplication.java
@@ -0,0 +1,13 @@
+package com.api.apitoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiToyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/apitoy/ApiToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apitoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
