8490e3f9b8004347e85640d8ac5fa964e37e2b4c
helloqlsghk@naver.com <helloqlsghk@naver.com>
2025-05-31 10:42:54 +0900

    SpringBoot 초기 셋팅중....

--- src/main/java/com/api/apitoy/api/domain/login/LoginController.java
+++ src/main/java/com/api/apitoy/api/domain/login/LoginController.java
@@ -1,13 +1,20 @@
 package com.api.apitoy.api.domain.login;
 
+import com.api.apitoy.api.utils.response.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
 
 
+    @PostMapping("/login")
+    public CommonResponse<?> login(){
+
+        return new CommonResponse<>("로그인 성공");
+    }
 
 
 
--- src/main/java/com/api/apitoy/api/utils/response/CommonResponse.java
+++ src/main/java/com/api/apitoy/api/utils/response/CommonResponse.java
@@ -1,20 +1,22 @@
 package com.api.apitoy.api.utils.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommonResponse<T> {
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-    private LocalDateTime time = LocalDateTime.now();
-    private String resultMessage = "SUCCESS";
+    private int code;
     private String statusCode = HttpStatus.OK.toString();
     private String message = "정상 처리입니다.";
 
